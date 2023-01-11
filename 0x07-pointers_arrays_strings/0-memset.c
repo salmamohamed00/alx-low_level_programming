dc94adc462dd4aadb0c43a5184f3fef71a1c44cb
salmahosny <salmamohamed19469@gmail.com>
2023-01-11 15:19:12 -0800

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * memset - Fills the first n bytes of the memory area
+ *_memset - Fills the first n bytes of the memory area
  *          pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
@@ -9,14 +8,12 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-
 void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
 	unsigned char *memory = s, value = c;
 
 	for (index = 0; index < n; index++)
-
 		memory[index] = value;
 
 	return (memory);
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -12,4 +13,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+#endif/*MAIN_H*/
